6412373d7cc8a9f56dc2327b08fbcc8916a48a18
Väinö Jalonen <vaino.jalonen@gmail.com>
2019-03-14 20:04:13 +0200

    cleanup

---
+++ Domain.cs
@@ -0,0 +1,94 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Domain 
+{
+
+    public class MemoryGame 
+    {
+        List<Card> cards;
+        Card firstGuess;
+        Card secondGuess;
+        Action onWin;
+
+        public MemoryGame(List<Card> cards, Action onWin)
+        {
+            this.cards = cards;
+            this.onWin = onWin;
+        }
+
+        public GameState State { get; private set; }
+        
+        public void Guess(Card card) 
+        {
+            if(State == GameState.GuessFirst && card.State == CardState.Hidden) 
+            {
+                card.State = CardState.Open;
+                firstGuess = card;
+                State = GameState.GuessSecond;
+                return;
+            }
+
+            if(State == GameState.GuessSecond && card.State == CardState.Hidden) 
+            {
+                card.State = CardState.Open;
+                secondGuess = card;
+                State = GameState.Continue;
+                return;
+            }
+
+            if(State == GameState.Continue) 
+            {
+                if(firstGuess.Url == secondGuess.Url)
+                {
+                    firstGuess.State = CardState.Found;
+                    secondGuess.State = CardState.Found;
+                    
+                } 
+                else 
+                {
+                    firstGuess.State = CardState.Hidden;
+                    secondGuess.State = CardState.Hidden;
+                }
+                State = GameState.GuessFirst;
+                return;
+            }
+
+            if (cards.All(c => c.IsFound))
+            {
+                State = GameState.Win;
+                onWin();
+            }
+        }
+    }
+    
+    public enum GameState {
+        GuessFirst,
+        GuessSecond,
+        Continue,
+        Win,
+        Lose
+    }
+
+    public class Card 
+    {
+        public string Url { get; set; }
+        public CardState State{ get;set; } = CardState.Hidden;
+
+        public void Clicked()  
+        {
+            if(State == CardState.Hidden)
+                State = CardState.Open;
+        }
+
+        public bool IsFlipped => State == CardState.Open || State == CardState.Found;
+        public bool IsFound => State == CardState.Found;
+    }
+
+    public enum CardState {
+        Hidden,
+        Open,
+        Found
+    }
+}
